04a061546c8932c7592cd560f36228a0ad783287
Marcos Buydid <marcosbuydid@gmail.com>
2026-03-15 15:21:50 -0300

    delete market risk monitor example

--- DesignPatterns/ObserverPattern.MarketRiskMonitor/Observer/IMarketObserver.cs
+++
@@ -1,9 +0,0 @@
-﻿using ObserverPattern.MarketRiskMonitor.DomainModel;
-
-namespace ObserverPattern.MarketRiskMonitor.Observer
-{
-    public interface IMarketObserver
-    {
-        void Update(MarketEvent marketEvent);
-    }
-}
