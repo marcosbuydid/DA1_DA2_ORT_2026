4ac9c1e2b5292b01c806176a26d8df9a15753b5e
marcos <marcosbuydid@gmail.com>
2026-03-15 22:27:32 -0300

    create facade pattern example

---
+++ DesignPatterns/FacadePattern.RocketLaunchSystem/Facade/RocketLaunchFacade.cs
@@ -0,0 +1,31 @@
+﻿
+using FacadePattern.RocketLaunchSystem.Subsystems;
+
+namespace FacadePattern.RocketLaunchSystem.Facade
+{
+    public class RocketLaunchFacade
+    {
+        private PropulsionSubsystem propulsionSubsystem = new PropulsionSubsystem();
+        private GuidanceSubsystem guidanceSubsystem = new GuidanceSubsystem();
+        private TelemetrySubsystem telemetrySubsystem = new TelemetrySubsystem();
+
+        public void PrepareLaunch()
+        {
+            Console.WriteLine("Rocket launch preparation started.");
+
+            Console.WriteLine();
+
+            propulsionSubsystem.ChillEngines();
+            propulsionSubsystem.PressurizeFuel();
+
+            guidanceSubsystem.AlignIMU();
+            guidanceSubsystem.LoadTrajectory();
+
+            telemetrySubsystem.InitializeLink();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Rocket is ready for launch.");
+        }
+    }
+}
---
+++ DesignPatterns/FacadePattern.RocketLaunchSystem/Subsystems/GuidanceSubsystem.cs
@@ -0,0 +1,15 @@
+﻿namespace FacadePattern.RocketLaunchSystem.Subsystems
+{
+    public class GuidanceSubsystem
+    {
+        public void AlignIMU()
+        {
+            Console.WriteLine("Inertial measurement unit aligned.");
+        }
+
+        public void LoadTrajectory()
+        {
+            Console.WriteLine("Flight trajectory loaded.");
+        }
+    }
+}
