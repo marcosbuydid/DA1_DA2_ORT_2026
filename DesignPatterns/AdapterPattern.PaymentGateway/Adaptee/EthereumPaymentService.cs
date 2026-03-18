00b3fccf8b9d46e4ab2837ca3a5fe7f75af76f12
marcos <marcosbuydid@gmail.com>
2026-03-17 21:28:53 -0300

    create adapter pattern example

---
+++ DesignPatterns/AdapterPattern.PaymentGateway/Adaptee/EthereumPaymentService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace AdapterPattern.PaymentGateway.Adaptee
+{
+    public class EthereumPaymentService
+    {
+        public string SendTransaction(decimal amount, string walletAddress)
+        {
+            return $"ETH_TX_{Guid.NewGuid()}";
+        }
+
+        public bool WaitForConfirmation(string transactionHash)
+        {
+            Console.WriteLine("Waiting for ethereum confirmation...");
+
+            Thread.Sleep(1000); // simulate delay
+
+            // simulate success/failure
+            return new Random().Next(0, 2) == 1;
+        }
+    }
+}
---
+++ DesignPatterns/AdapterPattern.PaymentGateway/Program.cs
@@ -0,0 +1,19 @@
+﻿using AdapterPattern.PaymentGateway.Adaptee;
+using AdapterPattern.PaymentGateway.Adapter;
+using AdapterPattern.PaymentGateway.Target;
+
+namespace AdapterPattern.PaymentGateway
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EthereumPaymentService ethereumPaymentService = new EthereumPaymentService();
+
+            IPaymentProcessor processor =
+                new EthereumPaymentGateway(ethereumPaymentService, "user-wallet-SDF76CVNJ87");
+
+            processor.ProcessPayment(159.45m);
+        }
+    }
+}
