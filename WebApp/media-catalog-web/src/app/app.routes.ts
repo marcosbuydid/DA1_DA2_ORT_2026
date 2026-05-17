4ff03ba027da0c27e131ed2702ccfd629f8ec1bf
marcos <marcosbuydid@gmail.com>
2026-05-17 18:09:42 -0300

    create login, home and not found components. configure routes

--- WebApp/media-catalog-web/src/app/app.routes.ts
+++ WebApp/media-catalog-web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+import { Home } from './features/home/pages/home/home';
+import { NotFound } from './shared/components/not-found/not-found';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'home', component: Home },
+    { path: '**', component: NotFound },
+];
