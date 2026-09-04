16035e69a3f5ae419f83da6f2f75eab263acb335
marcos <marcosbuydid@gmail.com>
2026-09-04 09:58:18 -0300

    implement movies fetch

--- WebApp/media-catalog-web/src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ WebApp/media-catalog-web/src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -8,10 +8,13 @@ import { SessionService } from '../../../../core/services/session.service';
 import { filter, take, switchMap } from 'rxjs';
 import { UserService } from '../../../users/user.service';
 import { UserDetailDTO } from '../../../auth/models/user-detail.dto';
+import { DatePipe } from '@angular/common';
+import { MovieDetailDTO } from '../../../auth/models/movie-detail.dto';
+import { MovieService } from '../../../movies/movie.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Navbar],
+  imports: [Navbar, DatePipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
@@ -20,6 +23,7 @@ export class Dashboard {
 
   roles: RoleDetailDTO[] = [];
   users: UserDetailDTO[] = [];
+  movies: MovieDetailDTO[] = [];
   section = 'home';
   loading = false;
 
@@ -28,12 +32,14 @@ export class Dashboard {
     private roleService: RoleService,
     private sessionService: SessionService,
     private userService: UserService,
+    private movieService: MovieService,
     private cdr: ChangeDetectorRef
   ) {
     this.route.paramMap.subscribe(params => {
       this.section = params.get('section') ?? 'home';
       this.roles = [];
       this.users = [];
+      this.movies = [];
 
       switch (this.section) {
         case 'roles':
@@ -43,6 +49,9 @@ export class Dashboard {
           break;
         case 'users':
           this.loadUsers();
+          break;
+        case 'movies':
+          this.loadMovies();
           break;
       }
     });
@@ -81,6 +90,28 @@ export class Dashboard {
       .subscribe({
         next: response => {
           this.users = response;
+          this.loading = false;
+          this.cdr.detectChanges();
+        },
+        error: error => {
+          console.error(error);
+          this.loading = false;
+          this.cdr.detectChanges();
+        }
+      });
+  }
+
+  private loadMovies(): void {
+    this.loading = true;
+    this.sessionService.session$
+      .pipe(
+        filter(session => session !== null),
+        take(1),
+        switchMap(() => this.movieService.getMovies())
+      )
+      .subscribe({
+        next: response => {
+          this.movies = response;
           this.loading = false;
           this.cdr.detectChanges();
         },
---
+++ WebApp/media-catalog-web/src/app/features/movies/movie.service.ts
@@ -0,0 +1,23 @@
+
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { MovieDetailDTO } from '../auth/models/movie-detail.dto';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MovieService {
+
+    private readonly http = inject(HttpClient);
+    private readonly apiUrl = `${environment.baseUrl}movies`;
+
+    getMovies(): Observable<MovieDetailDTO[]> {
+        return this.http
+            .get<{ result: MovieDetailDTO[] }>(this.apiUrl)
+            .pipe(
+                map(response => response.result)
+            );
+    }
+}
