426c322139bcb5c5bdf69ac1929674e972143972
marcos <marcosbuydid@gmail.com>
2026-09-15 20:38:40 -0300

    implement child routes, refactor dashboard component

--- WebApp/media-catalog-web/src/app/app.routes.ts
+++ WebApp/media-catalog-web/src/app/app.routes.ts
@@ -2,12 +2,32 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
 import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 import { NotFound } from './shared/components/not-found/not-found';
+import { RoleList } from './features/dashboard/components/role-list/role-list';
+import { AddRole } from './features/dashboard/components/add-role/add-role';
+import { UserList } from './features/dashboard/components/user-list/user-list';
+import { AddUser } from './features/dashboard/components/add-user/add-user';
+import { MovieList } from './features/dashboard/components/movie-list/movie-list';
+import { AddMovie } from './features/dashboard/components/add-movie/add-movie';
+import { ChangePassword } from './features/dashboard/components/change-password/change-password';
+import { Home } from './features/dashboard/components/home/home';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
     { path: 'auth/login', component: Login },
-    { path: 'dashboard/:section', component: Dashboard },
-    { path: 'dashboard', redirectTo: 'dashboard/home', pathMatch: 'full' },
+    {
+        path: 'dashboard', component: Dashboard,
+        children: [
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: Home },
+            { path: 'roles', component: RoleList },
+            { path: 'add-role', component: AddRole },
+            { path: 'users', component: UserList },
+            { path: 'add-user', component: AddUser },
+            { path: 'movies', component: MovieList },
+            { path: 'add-movie', component: AddMovie },
+            { path: 'change-password', component: ChangePassword },
+        ]
+    },
     { path: '**', component: NotFound },
 ];
